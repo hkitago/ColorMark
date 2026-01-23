1047ae785cf5146e0905074ede43c2f11d24b105
hkitago <hkitago@gmail.com>
2026-01-23 18:34:09 +0900

    Change to trigger syncColorMark on visibilitychange and skip onInstalled reload

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -1,16 +1,6 @@
 import { getCurrentLangLabelString } from './localization.js';
 import { isMacOS, getDefaultColor } from './utils.js';
 
-browser.runtime.onInstalled.addListener(async () => {
-  const tabs = await browser.tabs.query({});
-
-  for (const tab of tabs) {
-    if (tab.url.startsWith('http') || tab.url.startsWith('https')) {
-      await browser.tabs.reload(tab.id);
-    }
-  }
-});
-
 // ContextMenu for macOS
 if (isMacOS()) {
   browser.contextMenus.create({
@@ -30,17 +20,3 @@ if (isMacOS()) {
     }
   });
 }
-
-// Send a message to content.js on when switching tab
-browser.tabs.onActivated.addListener((activeInfo) => {
-  browser.tabs.sendMessage(activeInfo.tabId, { type: 'syncColorMark' })
-    .catch(error => console.error('Error getting page info:', error));
-});
-
-// Send a message to content.js when the tab's status is complete
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    browser.tabs.sendMessage(tabId, { type: 'syncColorMark' })
-      .catch(error => console.error('Error getting page info after update:', error));
-  }
-});
